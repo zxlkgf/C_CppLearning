fbb8057ca3fdbe5ae1e0d7d19aba92a7b638d376
zxlkgf <z514111073@gmail.com>
2023-08-10 22:59:09 +0800

    create ch14

---
+++ ch14/14.1-14.3/studentc.h
@@ -0,0 +1,35 @@
+// studentc.h -- defining a Student class using containment
+#ifndef STUDENTC_H_
+#define STUDENTC_H_
+
+#include <iostream>
+#include <string>
+#include <valarray>
+class Student
+{
+private:
+    typedef std::valarray<double> ArrayDb;
+    std::string name;
+    ArrayDb scores;
+    std::ostream & arr_out(std::ostream &os)const;
+public:
+    Student():name("Null Student")ArrayDb() {}
+    explicit Student(const std::string & s):name(s), ArrayDb() {}
+    explicit Student(int n) : name("Nully"), ArrayDb(n) {}
+    Student(const std::string & s, int n):name(s), ArrayDb(n) {}
+    Student(const std::string & s, const ArrayDb & a):name(s), ArrayDb(a) {}
+    Student(const char * str, const double * pd, int n):name(str), ArrayDb(pd, n) {}
+    ~Student() {}
+    double Average() const;
+    double & operator[](int i);
+    double operator[](int i) const;
+    const std::string & Name() const;
+// friends
+    // input
+    friend std::istream & operator>>(std::istream & is,Student & stu);  // 1 word
+    friend std::istream & getline(std::istream & is,Student & stu);     // 1 line
+    // output
+    friend std::ostream & operator<<(std::ostream & os,const Student & stu);
+};
+
+#endif
